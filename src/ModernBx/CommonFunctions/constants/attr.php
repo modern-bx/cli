8869b4b81f21a0b2b0f924690962dfa323afa9b4
whiskyjs <alexander.selyuchenko@gmail.com>
2026-07-16 20:38:37 +0300

    Vendor common helpers and remove boilerplate commands

---
+++ src/ModernBx/CommonFunctions/constants/attr.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ModernBx\CommonFunctions\Constants\Attr;
+
+const ATTR_PATTERN_STD = '{key}="{value}"';
+const ATTR_PATTERN_DATA = 'data-{key}="{value}"';
